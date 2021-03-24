caa5d23a464c1c75bac9c1c174e3c8d79f04656c
Olha Salatova <64421452+volyaosi@users.noreply.github.com>
2021-03-24 15:40:52 +0100

    Delete day 2 directory

--- day 2/task2.js
+++
@@ -1,9 +0,0 @@
-// Task 2:
-// Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
-
-function putInOrder(str) {
-  return str
-    .split(" ")
-    .sort((a, b) => a.match(/\d/)[0] - b.match(/\d/)[0])
-    .join();
-}
